39f72b6242bf73a78ea26bf320b5edf69bda8de9
tudao@5nhatnhat.com <nevarda9416>
2021-12-27 13:36:58 +0700

    - [EmberJS]: Component Basics + Passing content to components with {{yield}} + Using the application template and {{outlet}}

--- tests/acceptance/super-rentals-test.js
+++ tests/acceptance/super-rentals-test.js
@@ -8,6 +8,8 @@ module('Acceptance | super rentals', function (hooks) {
   test('visiting /', async function (assert) {
     await visit('/');
     assert.equal(currentURL(), '/');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('SuperRentals');
     assert.dom('h2').hasText('Welcome to Super Rentals!');
     assert.dom('.jumbo a.button').hasText('About Us');
     await click('.jumbo a.button');
@@ -17,6 +19,8 @@ module('Acceptance | super rentals', function (hooks) {
   test('visiting /about', async function (assert) {
     await visit('/about');
     assert.equal(currentURL(), '/about');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('SuperRentals');
     assert.dom('h2').hasText('About Super Rentals!');
     assert.dom('.jumbo a.button').hasText('Contact Us');
     await click('.jumbo a.button');
@@ -26,9 +30,26 @@ module('Acceptance | super rentals', function (hooks) {
   test('visiting /getting-in-touch', async function (assert) {
     await visit('/getting-in-touch');
     assert.equal(currentURL(), '/getting-in-touch');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('SuperRentals');
     assert.dom('h2').hasText('Contact Us');
     assert.dom('.jumbo a.button').hasText('Home');
     await click('.jumbo a.button');
     assert.equal(currentURL(), '/');
   });
+
+  test('navigating using the nav-bar', async function (assert) {
+    await visit('/');
+    assert.dom('nav').exists();
+    assert.dom('nav a.menu-index').hasText('SuperRentals');
+    assert.dom('nav a.menu-about').hasText('About');
+    assert.dom('nav a.menu-contact').hasText('Contact');
+    await click('nav a.menu-about');
+    assert.equal(currentURL(), '/about');
+    await click('nav a.menu-contact');
+    assert.equal(currentURL(), '/getting-in-touch');
+    await click('nav a.menu-index');
+    assert.equal(currentURL(), '/');
+  });
 });
+
